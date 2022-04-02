fee2b272ec1163286b3d8cda057410908651f473
Odai Athamneh <heyodai@gmail.com>
2022-04-02 17:11:22 -0500

    add dashboard component

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,7 @@
-// import styles from '../styles/Home.module.css'
+import Dashboard from '../components/dashboard';
 
 export default function Home() {
   return (
-    <div id="dashboard">
-      <div className="row">
-        <div className="col-sm-12">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-9">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-3">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      </div >
-    </div>
+    <Dashboard />
   )
 }
